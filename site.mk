3b2f0f99c216e7efcd573141c349fb431aaec404
Nils Schneider <nils@nilsschneider.net>
2014-07-26 22:58:48 +0200

    sync site.mk with ffhl

--- site.mk
+++ site.mk
@@ -5,21 +5,22 @@
 
 GLUON_SITE_PACKAGES := \
 	gluon-alfred \
+	gluon-announced \
 	gluon-autoupdater \
 	gluon-config-mode \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
-	gluon-next-node \
+	gluon-luci-portconfig \
 	gluon-mesh-batman-adv \
 	gluon-mesh-vpn-fastd \
+	gluon-next-node \
 	gluon-radvd \
 	gluon-status-page \
 	iwinfo \
 	iptables \
 	haveged
-
 
 ##	DEFAULT_GLUON_RELEASE
 #		version string to use for images
@@ -39,3 +40,6 @@ DEFAULT_GLUON_RELEASE := 0.4+0-exp$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+# Default priority for updates.
+GLUON_PRIORITY ?= 0
